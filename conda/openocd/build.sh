6dcef5cf0193e019d14dfb3f39552a7435ed4bc7
Tim 'mithro' Ansell <mithro@mithis.com>
2016-06-04 13:53:24 +1000

    Build in a "build" directory.

--- conda/openocd/build.sh
+++ conda/openocd/build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 ./bootstrap
-./configure --prefix="$PREFIX" \
-    --enable-ftdi
+mkdir build
+cd build
+../configure \
+  --prefix="$PREFIX" \
+  --enable-ftdi
 make -j$CPU_COUNT
 make install
 
